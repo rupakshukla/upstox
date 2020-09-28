ce5a44bc5095507edcec4134a3326a518d96258c
RUPAK <rupakshukla@RUPAKs-MacBook-Air.local>
2020-09-28 19:52:04 +0530

    Test Commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ var React = require("react");
 var Component = React.Component;
 
 //var CanvasJSReact = require('./canvasjs.react');
-var CanvasJS = CanvasJSReact.CanvasJS;
+let CanvasJS = CanvasJSReact.CanvasJS;
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 class App extends Component {
