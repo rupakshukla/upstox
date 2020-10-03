199a60b27fff0d1a51f8c0a018dec3437960eb09
RUPAK <rupakshukla@RUPAKs-MacBook-Air.local>
2020-10-03 10:39:13 +0530

    Live setup

--- src/views/Typography/Typography.js
+++ src/views/Typography/Typography.js
@@ -1,5 +1,5 @@
 import React from "react";
-// @material-ui/core components
+// @material-ui/core
 import { makeStyles } from "@material-ui/core/styles";
 // core components
 import Card from "../../components/Card/Card.js";
@@ -7,42 +7,7 @@ import CardHeader from "../../components/Card/CardHeader.js";
 import CardBody from "../../components/Card/CardBody.js";
 import Live from "../../components/Live/Live.js";
 
-const styles = {
-  typo: {
-    paddingLeft: "25%",
-    marginBottom: "40px",
-    position: "relative",
-  },
-  note: {
-    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
-    bottom: "10px",
-    color: "#c0c1c2",
-    display: "block",
-    fontWeight: "400",
-    fontSize: "13px",
-    lineHeight: "13px",
-    left: "0",
-    marginLeft: "20px",
-    position: "absolute",
-    width: "260px",
-  },
-  cardCategoryWhite: {
-    color: "rgba(255,255,255,.62)",
-    margin: "0",
-    fontSize: "14px",
-    marginTop: "0",
-    marginBottom: "0",
-  },
-  cardTitleWhite: {
-    color: "#FFFFFF",
-    marginTop: "0px",
-    minHeight: "auto",
-    fontWeight: "300",
-    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
-    marginBottom: "3px",
-    textDecoration: "none",
-  },
-};
+import styles from "../../assets/jss/dashboardStyle.js";
 
 const useStyles = makeStyles(styles);
 
@@ -55,7 +20,7 @@ export default function TypographyPage() {
       </CardHeader>
       <CardBody>
         <div className={classes.typo}>
-          <h1>Historical Chart</h1>
+          <h1>Live Chart</h1>
         </div>
         <Live></Live>
       </CardBody>
